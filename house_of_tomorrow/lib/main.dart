2e32051d537d2991a8e42f9c723710e8acad9d11
choeseonghyeon <chjcmy@naver.com>
2025-02-06 00:23:09 +0900

    다국어 구현, 나이트 모드 구현

--- house_of_tomorrow/lib/main.dart
+++ house_of_tomorrow/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:house_of_tomorrow/src/service/lang_service.dart';
+import 'package:house_of_tomorrow/src/service/theme_service.dart';
+import 'package:house_of_tomorrow/src/view/shopping/shopping_view.dart';
+import 'package:house_of_tomorrow/util/lang/generated/l10n.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ThemeService(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => LangService(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,13 +27,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+
+    // 여기에서 const materialapp 일 경우에는 값이 고정되어 런타임 시에 벼경 될수 없다
+    return MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      locale: context.watch<LangService>().locale,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello House Of Tomorrow"),
-        ),
-      ),
+      theme: context.watch<ThemeService>().themeData,
+      home: ShoppingView(),
     );
   }
 }
